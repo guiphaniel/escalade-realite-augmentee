7667201f7ef9ff2175d9882b2a987bf29e2495ca
guiphaniel <50924532+guiphaniel@users.noreply.github.com>
2021-11-06 23:58:14 +0100

    creating PoseDetector class

--- src/detectors/pose_detector.py
+++ src/detectors/pose_detector.py
@@ -1,6 +1,21 @@
 import cv2
 import mediapipe as mp
 
-def detectLandmarks(image, pose):
-        results = pose.process(image)
+class PoseDetector:
+  
+    # Initializing
+    def __init__(self):
+        self.mp_pose = mp.solutions.mediapipe.python.solutions.pose
+        self.pose = self.mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5)  
+        
+    # Deleting (Calling destructor)
+    def __del__(self):
+        self.pose.close()
+
+
+    def detectLandmarks(self, image):
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        results = self.pose.process(image)
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
         return results
+
