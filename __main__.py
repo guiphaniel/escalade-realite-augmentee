0784c77e30f3c61d8531d8c5fbdf0f97d640059e
guiphaniel <50924532+guiphaniel@users.noreply.github.com>
2021-11-28 16:06:22 +0100

    Player

--- __main__.py
+++ __main__.py
@@ -1,6 +1,7 @@
 import cv2
 import mediapipe as mp
 from src.controllers.utils.detectors import pose_detector
+from src.model.components.player import Player
 
 mp_drawing = mp.solutions.mediapipe.python.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.mediapipe.python.solutions.drawing_styles
@@ -8,7 +9,10 @@ mp_pose = mp.solutions.mediapipe.python.solutions.pose
 
 # For webcam input:
 cap = cv2.VideoCapture(0)
-singlePoseDetector = pose_detector.PoseDetector()
+landmarks_enum = mp.solutions.mediapipe.python.solutions.pose.PoseLandmark
+player1 = Player("Guilhem")
+player1PoseDetector = pose_detector.PoseDetector()
+
 
 while cap.isOpened():
     success, image = cap.read()
@@ -17,20 +21,24 @@ while cap.isOpened():
         # If loading a video, use 'break' instead of 'continue'.
         continue
     
-    results = singlePoseDetector.detectLandmarks(image)
+    player1Landmarks = player1PoseDetector.detectLandmarks(image)   
 
-    if not results:
+    if not player1Landmarks:          
         continue
+    
+    print(player1Landmarks.landmark[landmarks_enum.NOSE].x)
+    
+    
     # Draw the pose annotation on the image.    
     mp_drawing.draw_landmarks(
         image,
-        results,
+        player1Landmarks,
         mp_pose.POSE_CONNECTIONS,
         landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
     # Flip the image horizontally for a selfie-view display.
     cv2.imshow('MediaPipe Pose', cv2.flip(image, 1))
     if cv2.waitKey(5) & 0xFF == 27:
         break
-del singlePoseDetector
+del player1PoseDetector
 cap.release()
 
--- src/model/components/player.py
+++ src/model/components/player.py
@@ -1,3 +1,29 @@
 #stocke les informations du joueur(nom, points, coordonnees...)
 
+import mediapipe as mp
+from mediapipe.framework.formats import landmark_pb2
+
+class Player:
+    #static variables
+    id = 0
+    
+    # Initializing
+    def __init__(self, pseudo):        
+        self.id = Player.id
+        Player.id += 1
+        self.pseudo = pseudo
+        
+    def getId(self):
+        return self.id
+    
+    def getPseudo(self):
+        return self.pseudo
+    
+    def setPseudo(self, pseudo):
+        self.pseudo = pseudo
+    
+    
+
+
+
 
