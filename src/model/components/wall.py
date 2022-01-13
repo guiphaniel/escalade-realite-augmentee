98bc1c062536bcba098d857c18a7328f26005178
guiphaniel <50924532+guiphaniel@users.noreply.github.com>
2022-01-13 15:42:48 +0100

    adding name

--- src/model/components/wall.py
+++ src/model/components/wall.py
@@ -3,5 +3,12 @@
 class Wall:
     def __init__(self):
         self.id = None
+        self.name = None
+        self.paths = []
+        self.handles = []
+
+    def __init__(self, name):
+        self.id = None
+        self.name = name
         self.paths = []
         self.handles = []
