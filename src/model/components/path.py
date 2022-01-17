2d41cf9bb60a2660f6477e28dfb3b0e35d172089
guiphaniel <50924532+guiphaniel@users.noreply.github.com>
2022-01-17 16:00:16 +0100

    fix missing capital letter

--- src/model/components/path.py
+++ src/model/components/path.py
@@ -1,6 +1,6 @@
 #TODO: stocke un nom/id, une liste de prises selectionnees, et une liste de joueurs ayant joué sur ce parcours
 
-class path:
+class Path:
     def __init__(self):
         self.id = None
         self.handles = []
