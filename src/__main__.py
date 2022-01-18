369ca7e56b69a0228959e41ef6199bb41dfb5dae
guiphaniel <50924532+guiphaniel@users.noreply.github.com>
2022-01-18 16:05:34 +0100

    display img cap

--- src/__main__.py
+++ src/__main__.py
@@ -1,9 +1,22 @@
+import cv2
+
+from src.controllers.utils.camera import Camera
 from src.model.components.handle import Handle
 from src.model.components.path import Path
 from src.model.components.wall import Wall
 from src.model.database import Database
 #
-cam = Camera()
+cam = Camera(0)
+
+while True:
+    
+    success, img = cam.read()
+    if not success:
+        continue
+
+    cv2.namedWindow('camera', cv2.WND_PROP_FULLSCREEN)
+    cv2.setWindowProperty('camera', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+    cv2.imshow('camera', img)
 
 
 
