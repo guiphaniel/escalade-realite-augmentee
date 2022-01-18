4eb36124b39c780f8152a9b555fb5974638210ea
guiphaniel <50924532+guiphaniel@users.noreply.github.com>
2022-01-18 16:28:23 +0100

    tests

--- src/__main__.py
+++ src/__main__.py
@@ -6,17 +6,20 @@ from src.model.components.path import Path
 from src.model.components.wall import Wall
 from src.model.database import Database
 #
+
 cam = Camera(0)
 
 while True:
-    
+    print("ratio")
     success, img = cam.read()
     if not success:
         continue
 
-    cv2.namedWindow('camera', cv2.WND_PROP_FULLSCREEN)
-    cv2.setWindowProperty('camera', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
     cv2.imshow('camera', img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cv2.destroyAllWindows()
 
 
 
