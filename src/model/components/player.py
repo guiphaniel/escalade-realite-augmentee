bb1cbbcf9028975804cb8a4440fc30bbdabc9733
guiphaniel <50924532+guiphaniel@users.noreply.github.com>
2022-01-19 13:31:57 +0100

    reformatting file

--- src/model/components/player.py
+++ src/model/components/player.py
@@ -1,27 +1,21 @@
-#stocke les informations du joueur(nom, points, coordonnees...)
+# stocke les informations du joueur(nom, points, coordonnees...)
 
 import mediapipe as mp
 from mediapipe.framework.formats import landmark_pb2
 
+
 class Player:
 
     # Initializing
-    def __init__(self, pseudo = "no_name"):
+    def __init__(self, pseudo="no_name"):
         self.id = None
         self.pseudo = pseudo
 
-        
     def getId(self):
         return self.id
-    
+
     def getPseudo(self):
         return self.pseudo
-    
+
     def setPseudo(self, pseudo):
         self.pseudo = pseudo
-    
-    
-
-
-
-
