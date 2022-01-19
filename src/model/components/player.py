e13cb720ec1fc726f0ae5e038acccdddb925358f
guiphaniel <50924532+guiphaniel@users.noreply.github.com>
2022-01-19 13:48:38 +0100

    remove useless getters

--- src/model/components/player.py
+++ src/model/components/player.py
@@ -7,15 +7,6 @@ from mediapipe.framework.formats import landmark_pb2
 class Player:
 
     # Initializing
-    def __init__(self, pseudo="no_name"):
+    def __init__(self, pseudo=None):
         self.id = None
-        self.pseudo = pseudo
-
-    def getId(self):
-        return self.id
-
-    def getPseudo(self):
-        return self.pseudo
-
-    def setPseudo(self, pseudo):
         self.pseudo = pseudo
