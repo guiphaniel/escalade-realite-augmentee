858ec348d98aaeba0543628a591caecfd27c2b7e
guiphaniel <50924532+guiphaniel@users.noreply.github.com>
2022-01-20 11:41:53 +0100

    more tests

--- src/__main__.py
+++ src/__main__.py
@@ -1,8 +1,25 @@
-from src.view.manager import Manager
+from src.model.components.handle import Handle
+from src.model.components.path import Path
+from src.model.components.wall import Wall
+from src.model.database import Database
 
-def processMenu():
-    disp = Manager(1920,1080)
-    disp.start()
+walls = Database().getWalls()
+print(walls)
+print(Database().getPathInWall(walls[0]))
 
-processMenu()
-
+# database = Database()
+# handle1 = Handle(5, 6)
+# handle2 = Handle(4, 10)
+# handles = [handle1, handle2]
+# wall1 = Wall()
+# wall2 = Wall("mon super mur")
+# database.setWalls([wall1, wall2])
+# database.setHandlesInWall(handles, wall1)
+# path1 = Path()
+# path2 = Path("nom")
+# database.setPathsInWall([path1, path2], wall1)
+# freePath = Path()
+# database.setPathsInWall([freePath, Path(), Path()], None)
+# database.setHandlesInPath([handle1], path1)
+# database.setHandlesInPath([handle1, handle2], path2)
+# database.setHandlesInPath([Handle(5, 4), Handle(1, 7)], freePath)
