364725f9bb2c15716b2d83264aab397aff9f94ed
guiphaniel <50924532+guiphaniel@users.noreply.github.com>
2022-01-26 10:42:20 +0100

    create frames (abstract and home)

---
+++ src/view/utils/frames/abstract_frame.py
@@ -0,0 +1,46 @@
+from abc import abstractmethod
+
+import pygame
+import src
+
+
+class AbstractFrame:
+    def __init__(self, bgColor, bgImage):
+        self.bgColor = bgColor
+        if bgImage:
+            self.bgImage = pygame.image.load(bgImage)
+        else:
+            self.bgImage = None
+        self.win = src.view.window.Window().win
+        self.items = []
+
+    @abstractmethod
+    def execute(self):
+        pass
+
+    def add(self, item):
+        self.items.append(item)
+
+    def remove(self, item):
+        self.items.remove(item)
+
+    def removeAll(self):
+        self.items.clear()
+
+    def repaint(self, item):
+        skip = True
+        for i in self.items:
+            if i == item:
+                skip = False
+            if not skip:
+                i.draw()
+        pygame.display.flip()
+
+    def repaintAll(self):
+        if self.bgImage:
+            self.win.blit(self.bgImage, (0, 0))
+        else:
+            self.win.fill(self.bgColor)
+        for i in self.items:
+            i.draw()
+        pygame.display.flip()
