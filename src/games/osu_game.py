1df23a3947140432db8c14adde9a7e64cf7f688f
baptistestubljar <baptiste.stubljar@gmail.com>
2022-01-31 17:13:44 +0100

    remove while loop

--- src/games/osu_game.py
+++ src/games/osu_game.py
@@ -10,43 +10,41 @@ class OsuGame(Game):
 
     def __init__(self):
         super().__init__()
+        self.score = 0
+        self.initTimeTarget = pygame.time.get_ticks()
+        self.waitTimeTarget = random.randint(2000, 5000)
+        self.targets = []
+        self.targetsDispawned = []
 
     def execute(self):
-        running = True
-        score = 0
-        initTimeTarget = pygame.time.get_ticks()
-        waitTimeTarget = random.randint(2000, 5000)
-        targets = []
-        targetsDispawned = []
-        while running:
-            self.win.fill((0, 0, 0, 0))
-            if (initTimeTarget + waitTimeTarget - pygame.time.get_ticks()) <= 0:
-                targets.append(Target())
-                initTimeTarget = pygame.time.get_ticks()
-                waitTimeTarget = random.randint(2000, 4000)
+        self.win.fill((0, 0, 0))
+        if (self.initTimeTarget + self.waitTimeTarget - pygame.time.get_ticks()) <= 0:
+            self.targets.append(Target())
+            self.initTimeTarget = pygame.time.get_ticks()
+            self.waitTimeTarget = random.randint(2000, 4000)
 
-            for t in targets:
-                t.draw()
-                if t.ticks + 5000 - pygame.time.get_ticks() <= 0:
-                    targets.remove(t)
-                    targetsDispawned.append(t)
+        for t in self.targets:
+            t.draw()
+            if t.ticks + 5000 - pygame.time.get_ticks() <= 0:
+                self.targets.remove(t)
+                self.targetsDispawned.append(t)
+                t.ticks = pygame.time.get_ticks()
+                t.failed()
+
+        positionPlayer = self.getPlayerPosition()
+
+        for position in positionPlayer:
+            for t in self.targets:
+                if t.collide(position):
+                    self.targets.remove(t)
+                    self.targetsDispawned.append(t)
                     t.ticks = pygame.time.get_ticks()
-                    t.failed()
+                    self.score += 1
+                    print(self.score)
 
-            positionPlayer = self.getPlayerPosition()
+        for t in self.targetsDispawned:
+            t.draw()
+            if t.ticks + 2000 - pygame.time.get_ticks() <=0:
+                self.targetsDispawned.remove(t)
 
-            for position in positionPlayer:
-                for t in targets:
-                    if t.collide(position):
-                        targets.remove(t)
-                        targetsDispawned.append(t)
-                        t.ticks = pygame.time.get_ticks()
-                        score += 1
-                        print(score)
-
-            for t in targetsDispawned:
-                t.draw()
-                if t.ticks + 2000 - pygame.time.get_ticks() <=0:
-                    targetsDispawned.remove(t)
-
-            pygame.display.flip()
+        pygame.display.flip()
