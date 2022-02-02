9941d7cca477e8aab8f9db46d5cf9eab3f21aefc
guiphaniel <50924532+guiphaniel@users.noreply.github.com>
2022-02-02 16:07:31 +0100

    remove unused ActionListener Interface

--- src/view/frames/targets_frame.py
+++ src/view/frames/targets_frame.py
@@ -3,7 +3,7 @@ from src.view.frames.abstract_frame import AbstractFrame
 from src.view.listeners.action_listener import ActionListener
 
 
-class TargetsFrame(AbstractFrame, ActionListener):
+class TargetsFrame(AbstractFrame):
     def __init__(self):
         AbstractFrame.__init__(self)
 
@@ -11,7 +11,3 @@ class TargetsFrame(AbstractFrame, ActionListener):
 
     def execute(self):
         self.targetGame.execute()
-
-    # TODO: use controllers, having window imbedded
-    def actionPerformed(self, src):
-        pass
