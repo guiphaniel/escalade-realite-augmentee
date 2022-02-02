a2a385f558649d196204bf20ba3b50be9389540f
guiphaniel <50924532+guiphaniel@users.noreply.github.com>
2022-02-02 16:08:19 +0100

    handle listeners for all buttons

--- src/view/frames/games_frame.py
+++ src/view/frames/games_frame.py
@@ -1,28 +1,28 @@
 import src
+from src.controllers.switch_frame_controller import SwitchFrameController
 from src.view.frames.abstract_frame import AbstractFrame
 from src.view.frames.targets_frame import TargetsFrame
 from src.view.items.button import Button
 from src.view.listeners.action_listener import ActionListener
-
+from src.view.frames.pong_frame import PongFrame
 
 class GamesFrame(AbstractFrame, ActionListener):
     def __init__(self):
         AbstractFrame.__init__(self, bgImage="view/images/background.png")
-        self.targetButton = Button(300, 400, text="CIBLES")
-        self.add(self.targetButton)
-        self.parcoursButton = Button(300, 700, text="PARCOURS")
-        self.add(self.parcoursButton)
         self.pongButton = Button(300, 100, text="PONG")
         self.add(self.pongButton)
+        self.targetButton = Button(300, 400, text="CIBLES")
+        self.add(self.targetButton)
+        self.pathButton = Button(300, 700, text="PARCOURS")
+        self.add(self.pathButton)
         self.returnButton = Button(1551, 919, text="RETOUR")
         self.add(self.returnButton)
         self.handlesDetectorButton = Button(900, 550, text="DETECTION PRISES")
         self.add(self.handlesDetectorButton)
 
-
-        self.targetButton.addActionListener(self)
-        self.parcoursButton.addActionListener(self)
         self.pongButton.addActionListener(self)
+        self.targetButton.addActionListener(self)
+        self.pathButton.addActionListener(self)
         self.returnButton.addActionListener(self)
         self.handlesDetectorButton.addActionListener(self)
 
@@ -31,6 +31,12 @@ class GamesFrame(AbstractFrame, ActionListener):
 
     #TODO: use controllers, having window imbedded
     def actionPerformed(self, source):
-        window = src.view.window.Window()
-        if source == self.targetButton:
-            window.currentFrame = TargetsFrame()
+        if source == self.pongButton:
+            SwitchFrameController().control(frame=PongFrame())
+        elif source == self.targetButton:
+            SwitchFrameController().control(frame=TargetsFrame())
+        elif source == self.pathButton:
+            SwitchFrameController().control(frame=PathFrame())
+        elif source == self.returnButton:
+            from src.view.frames.home_frame import HomeFrame
+            SwitchFrameController().control(frame=HomeFrame())
