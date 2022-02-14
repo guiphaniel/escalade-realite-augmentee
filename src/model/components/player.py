a005af0d9f3e90b8cce36416dca20b7f91929066
guiphaniel <50924532+guiphaniel@users.noreply.github.com>
2022-02-14 21:19:41 +0100

    making Player an Item (draw method)

--- src/model/components/player.py
+++ src/model/components/player.py
@@ -1,12 +1,54 @@
 # stocke les informations du joueur(nom, points, coordonnees...)
+import threading
 
 import mediapipe as mp
+import pygame
 from mediapipe.framework.formats import landmark_pb2
 
+from src.view.items.item import Item
 
-class Player:
+mp_pose = mp.solutions.pose
+
+
+class Player(Item):
 
     # Initializing
-    def __init__(self, pseudo=None):
+    def __init__(self, parent, pseudo=None):
+        Item.__init__(self, parent)
         self.id = None
         self.pseudo = pseudo
+
+        self.landmarks = {mp_pose.PoseLandmark.LEFT_WRIST: [],
+                          mp_pose.PoseLandmark.RIGHT_WRIST: [],
+                          mp_pose.PoseLandmark.LEFT_ANKLE: [],
+                          mp_pose.PoseLandmark.RIGHT_ANKLE: []}
+
+        self.mutexes = {mp_pose.PoseLandmark.LEFT_WRIST: threading.Lock(),
+                        mp_pose.PoseLandmark.RIGHT_WRIST: threading.Lock(),
+                        mp_pose.PoseLandmark.LEFT_ANKLE: threading.Lock(),
+                        mp_pose.PoseLandmark.RIGHT_ANKLE: threading.Lock()}
+
+        self.limbs = {}
+
+    def draw(self):
+        self.limbs[mp_pose.PoseLandmark.LEFT_WRIST] = self.__drawLimb(mp_pose.PoseLandmark.LEFT_WRIST)
+        self.limbs[mp_pose.PoseLandmark.RIGHT_WRIST] = self.__drawLimb(mp_pose.PoseLandmark.RIGHT_WRIST)
+        self.limbs[mp_pose.PoseLandmark.LEFT_ANKLE] = self.__drawLimb(mp_pose.PoseLandmark.LEFT_ANKLE)
+        self.limbs[mp_pose.PoseLandmark.RIGHT_ANKLE] = self.__drawLimb(mp_pose.PoseLandmark.RIGHT_ANKLE)
+
+
+    def __drawLimb(self, id):
+        self.mutexes[id].acquire()
+        ls = self.landmarks[id]
+
+        for l in ls:
+            l.x *= 1920
+            l.y *= 1080
+
+        limb = None
+        if ls:
+            if((max([l.x for l in ls]) - min([l.x for l in ls])) * (max([l.y for l in ls]) - min([l.y for l in ls])) < 10000):
+                limb = pygame.draw.polygon(self.win, (0, 0, 255), [(l.x, l.y) for l in ls])
+        self.mutexes[id].release()
+
+        return limb
