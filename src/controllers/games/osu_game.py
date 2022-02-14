9633a5eb45cdf85ec4413f9653fc4a6346df3857
guiphaniel <50924532+guiphaniel@users.noreply.github.com>
2022-02-14 21:21:26 +0100

    adapt to fit new MVC (frames' add method) + player landmarks

--- src/controllers/games/osu_game.py
+++ src/controllers/games/osu_game.py
@@ -17,23 +17,26 @@ class OsuGame(GameSinglePlayer):
 
     def execute(self):
         while self.continueGame:
-            self.win.fill((0, 0, 0))
             if (self.initTimeTarget + self.waitTimeTarget - pygame.time.get_ticks()) <= 0:
-                self.targets.append(Target(self.parent))
+                target = Target(self.parent)
+                self.targets.append(target)
+                self.parent.add(target)
                 self.initTimeTarget = pygame.time.get_ticks()
                 self.waitTimeTarget = random.randint(2000, 4000)
 
             for t in self.targets:
-                t.draw()
                 if t.ticks + 5000 - pygame.time.get_ticks() <= 0:
                     self.targets.remove(t)
                     self.targetsDispawned.append(t)
                     t.ticks = pygame.time.get_ticks()
                     t.failed()
 
-            for position in list(self.playerPosition.values()):
+            for limb in self.player.limbs.values():
+                if not limb:
+                    continue
+
                 for t in self.targets:
-                    if t.collide(position):
+                    if t.collide(limb):
                         self.targets.remove(t)
                         self.targetsDispawned.append(t)
                         t.ticks = pygame.time.get_ticks()
@@ -41,8 +44,8 @@ class OsuGame(GameSinglePlayer):
                         print(self.score)
 
             for t in self.targetsDispawned:
-                t.draw()
                 if t.ticks + 2000 - pygame.time.get_ticks() <=0:
                     self.targetsDispawned.remove(t)
+                    self.parent.remove(t)
 
-            pygame.display.flip()
+            self.parent.repaintAll()
