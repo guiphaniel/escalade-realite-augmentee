cb32322ae5e8242485bb69ab58fccc3b545214c4
guiphaniel <50924532+guiphaniel@users.noreply.github.com>
2022-02-23 19:58:45 +0100

    making handle drawable

--- src/model/components/handle.py
+++ src/model/components/handle.py
@@ -1,6 +1,19 @@
-class Handle:
-    def __init__(self, x, y):
+import pygame
+
+from src.view.items.drawable import Drawable
+
+
+class Handle(Drawable):
+    radius = 20
+
+    def __init__(self, parent, x, y):
         self.id = None
         self.x = x
         self.y = y
         self.radius = 20
+
+        Drawable.__init__(self, parent, x, y, self.radius, self.radius)
+
+    def draw(self):
+        self.rect = pygame.draw.circle(self.win, (50, 50, 50), self.rect.center, self.radius, 5)
+
