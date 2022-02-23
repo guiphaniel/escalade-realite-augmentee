93fc6122f1f553d036a965fa4e0f606afb0bea1d
guiphaniel <50924532+guiphaniel@users.noreply.github.com>
2022-02-23 11:02:39 +0100

    fix: check if the camera isn't obstructed during surface detection

--- src/utils/detectors/surface_detector.py
+++ src/utils/detectors/surface_detector.py
@@ -25,7 +25,11 @@ class Surface:
         index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
         search_params = dict(checks=50)
         flann = cv2.FlannBasedMatcher(index_params, search_params)
-        matches = flann.knnMatch(des1, des2, k=2)
+        try:
+            matches = flann.knnMatch(des1, des2, k=2)
+        except:
+            print("The camera is obstructed")
+            return False, None
         # store all the good matches as per Lowe's ratio test.
         good = []
         for m, n in matches:
