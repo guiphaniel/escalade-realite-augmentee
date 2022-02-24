50b4b0bd05a07c01b9d1a48d692f68ee4f7cab06
guiphaniel <50924532+guiphaniel@users.noreply.github.com>
2022-02-24 12:27:28 +0100

    fix bad method name

--- src/utils/events/keyboard_listener.py
+++ src/utils/events/keyboard_listener.py
@@ -5,7 +5,7 @@ from src.utils.events.event_manager import EventManager
 
 class KeyboardListener:
     def __init__(self):
-        EventManager().addKeyboardListeners(self)
+        EventManager().addKeyboardListener(self)
 
     #needs to return True if the event is caught (i.e. the element is concerned by the event)
     @abstractmethod
