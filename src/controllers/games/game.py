d0733933602044aafa923a428f6bfee429d50d4b
guiphaniel <50924532+guiphaniel@users.noreply.github.com>
2022-02-24 12:28:03 +0100

    use super constructor

--- src/controllers/games/game.py
+++ src/controllers/games/game.py
@@ -1,25 +1,17 @@
-import threading
-from abc import abstractmethod
-import cv2
-import numpy as np
 import pygame.draw
 
 import src
 from src.controllers.abstract_controller import AbstractController
 from src.controllers.switch_frame_controller import SwitchFrameController
-from src.utils.camera import Camera
-from src.utils.detectors import pose_detector
-from src.utils.transform import Transform
-from src.utils.events.event_manager import EventManager
 from src.utils.events.keyboard_listener import KeyboardListener
 
 
 class Game(AbstractController, KeyboardListener):
 
     def __init__(self, parent):
+        KeyboardListener.__init__(self)
         self.parent = parent
         self.win = src.view.window.Window().win
-        EventManager().addKeyboardListener(self)
         self.continueGame = True
 
     def onKeyboardEvent(self, e):
