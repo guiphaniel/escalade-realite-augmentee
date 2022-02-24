2ea2f2bc4f00f21a0ca6f3cd3ef7bf3203fecf6a
guiphaniel <50924532+guiphaniel@users.noreply.github.com>
2022-02-24 14:47:42 +0100

    override repaintAll method to display handles index

--- src/view/frames/path_frame.py
+++ src/view/frames/path_frame.py
@@ -1,7 +1,24 @@
-from src.controllers.games.path_game import PathGame
+import pygame
+
+from src.model.components.handle import Handle
 from src.view.frames.abstract_frame import AbstractFrame
 
 
 class PathFrame(AbstractFrame):
     def __init__(self):
         AbstractFrame.__init__(self)
+
+    def repaintAll(self):
+        if self.bgImage:
+            self.win.blit(self.bgImage, (0, 0))
+        else:
+            self.win.fill(self.bgColor)
+        cpt = 1
+        for i in self.items:
+            i.draw()
+            if isinstance(i, Handle):
+                font = pygame.font.SysFont("view/fonts/All the Way to the Sun.otf", 40)
+                textSurface = font.render(str(cpt), True, (255, 255, 255))
+                self.win.blit(textSurface, i.rect.bottomright)
+                cpt += 1
+        pygame.display.flip()
