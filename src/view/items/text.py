65cfbb7b7ce304cceec2cea09cd5691c0a91e413
guiphaniel <50924532+guiphaniel@users.noreply.github.com>
2022-02-24 12:34:57 +0100

    fix name and add default method values

--- src/view/items/text.py
+++ src/view/items/text.py
@@ -5,7 +5,7 @@ from src.view.items.drawable import Drawable
 
 
 # TODO: rajouter la gestion d'une bgImage
-class Button(Drawable):
+class Text(Drawable):
     def __init__(self, parent, x, y, text, textColor=(0, 0, 0), textSize=40,
                  textFont="view/fonts/All the Way to the Sun.otf"):
         Drawable.__init__(self, parent)
@@ -13,7 +13,8 @@ class Button(Drawable):
         self.rect = pygame.rect.Rect(x, y, 0, 0)
         self.setText(text, textColor, textSize, textFont)
 
-    def setText(self, text, textColor, textSize, textFont):
+    def setText(self, text, textColor=(0, 0, 0), textSize=40,
+                 textFont="view/fonts/All the Way to the Sun.otf"):
         font = pygame.font.SysFont(textFont, textSize)
         self.__textSurface = font.render(text, True, textColor)
         self.rect.w, self.rect.h = self.__textSurface.get_rect().topleft
