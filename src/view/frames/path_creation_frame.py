eec4f3df3eaa2c531bdc4f1725a029eba2d237b3
guiphaniel <50924532+guiphaniel@users.noreply.github.com>
2022-02-24 14:47:16 +0100

    adding method for back and valid + check if no handles + is keyboard listener (to escape)

--- src/view/frames/path_creation_frame.py
+++ src/view/frames/path_creation_frame.py
@@ -1,40 +1,55 @@
 import pygame
 
+import src
 from src.controllers.games.path_game import PathGame
+from src.controllers.start_game_controller import StartGameController
+from src.controllers.switch_frame_controller import SwitchFrameController
 from src.model.components.handle import Handle
+from src.model.components.path import Path
+from src.model.components.player import Player
+from src.model.database import Database
+from src.utils.events.keyboard_listener import KeyboardListener
 from src.utils.events.motion_listener import MotionListener
 from src.utils.events.mouse_listener import MouseListener
 from src.view.frames.abstract_frame import AbstractFrame
+from src.view.frames.path_frame import PathFrame
 from src.view.internalframes.handlesEditorInternalFrame import HandleEditorInternalFrame
 
 eM = HandleEditorInternalFrame.EditorMode
 
 
-class PathCreationFrame(AbstractFrame, MouseListener, MotionListener):
+class PathCreationFrame(AbstractFrame, MouseListener, MotionListener, KeyboardListener):
 
     def __init__(self):
         AbstractFrame.__init__(self)
         MouseListener.__init__(self)
         MotionListener.__init__(self)
+        KeyboardListener.__init__(self)
+
+        self.path = Path()
+        Database().setPathsInWall([self.path], None)
 
         self.editorMode = eM.ADD
         self.lastMousePosX = None
         self.lastMousePosY = None
         self.editedHandle = None
 
-        self.add(HandleEditorInternalFrame(self, (10, 10)))
+        self.editorFrame = HandleEditorInternalFrame(self, (10, 10))
+        self.add(self.editorFrame)
 
     def onMouseEvent(self, e) -> bool:
         pos = pygame.mouse.get_pos()
         if e.type == pygame.MOUSEBUTTONDOWN:
             if self.editorMode == eM.ADD:
                 self.add(Handle(self, pos[0] - Handle.radius / 2, pos[1] - Handle.radius / 2))
+                self.__isHandlesEmpty()
                 self.repaintAll()
                 return True
             elif self.editorMode == eM.REMOVE:
                 handle = self.findHandle(pos)
                 if handle:
                     self.remove(handle)
+                    self.__isHandlesEmpty()
                     self.repaintAll()
                     return True
 
@@ -48,7 +63,11 @@ class PathCreationFrame(AbstractFrame, MouseListener, MotionListener):
 
         return False
 
-        return True
+    def __isHandlesEmpty(self):
+        if [h for h in self.items if isinstance(h, Handle)]:
+            self.editorFrame.validBt.active = True
+        else:
+            self.editorFrame.validBt.active = False
 
     def onMotionEvent(self, e) -> bool:
         if self.editedHandle:
@@ -63,6 +82,19 @@ class PathCreationFrame(AbstractFrame, MouseListener, MotionListener):
 
         return False
 
+    def onBackBt(self):
+        from src.view.frames.games_frame import GamesFrame
+        SwitchFrameController().execute(frame=GamesFrame())
+
+    def onValidBt(self):
+        handles = [h for h in self.items if isinstance(h, Handle)]
+        self.path.setHandles(handles)
+        Database().setHandlesInPath(handles, self.path)
+
+        pathFrame = PathFrame()
+        SwitchFrameController().execute(frame=pathFrame)
+        StartGameController().execute(game=PathGame(pathFrame, self.path, Player(pathFrame)))
+
     def findHandle(self, coordinates):
         for h in self.items:
             if isinstance(h, Handle) and h.rect.collidepoint(coordinates):
@@ -73,7 +105,7 @@ class PathCreationFrame(AbstractFrame, MouseListener, MotionListener):
             self.win.blit(self.bgImage, (0, 0))
         else:
             self.win.fill(self.bgColor)
-        cpt = 0
+        cpt = 1
         for i in self.items:
             i.draw()
             if isinstance(i, Handle):
@@ -82,3 +114,10 @@ class PathCreationFrame(AbstractFrame, MouseListener, MotionListener):
                 self.win.blit(textSurface, i.rect.bottomright)
                 cpt += 1
         pygame.display.flip()
+
+    def onKeyboardEvent(self, e):
+        if e.key == pygame.K_ESCAPE or e.type == pygame.QUIT:
+            SwitchFrameController().execute(frame=src.view.frames.games_frame.GamesFrame())
+            return True
+
+        return False
