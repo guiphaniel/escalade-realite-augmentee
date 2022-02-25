14b9c3207130153254738d0b2c8f2143b195042f
guiphaniel <50924532+guiphaniel@users.noreply.github.com>
2022-02-25 16:03:06 +0100

    remove duplicated Singleton class

--- src/utils/camera.py
+++ src/utils/camera.py
@@ -1,13 +1,6 @@
 import cv2
 
-
-class Singleton(type):
-    _instances = {}
-
-    def __call__(cls, *args, **kwargs):
-        if cls not in cls._instances:
-            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
-        return cls._instances[cls]
+from src.utils.Singleton import Singleton
 
 
 class Camera(metaclass=Singleton):
